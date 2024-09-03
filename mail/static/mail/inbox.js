b45780ac02e22e2fe56da8e9837c5f37aded24c3
Ayrton Machado <ayrtonsoaresm@gmail.com>
2024-09-02 22:38:38 -0300

    fixed: order of emails in the mailbox, latest emails are shown

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -8,14 +8,14 @@ document.addEventListener('DOMContentLoaded', function() {
   
   //Submit Email
   document.querySelector('#compose-form').onsubmit = function() {
-    submit_email();
-    load_mailbox('sent')
+    submit_email()
+    setInterval(load_mailbox('sent'), 1000);
 
     return false;
   };
   
   // By default, load the inbox
-  load_mailbox('inbox');
+  setInterval(load_mailbox('inbox'), 1000);
 });
 
 function submit_email() {
@@ -39,7 +39,6 @@ function compose_email() {
   
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
-  document.querySelector('#emails-body').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
   
   // Clear out composition fields
@@ -52,26 +51,37 @@ function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
-  document.querySelector('#emails-body').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
   
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
   
   //API get emails
-  fetch(`emails/${mailbox}`)
+  fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {
     
     // Show the mailbox e-mails
-    emails.forEach(function(each_email) {
-      fetch(`emails/${each_email.id}`)
-      .then(response => response.json())
-      .then(email => {
-        console.log(email);
-        //Need to add a div to each email, to show correctly in mailbox
-        document.querySelector('#emails-body').append("()")
-      });
+    emails.forEach(function(each_email, index) {
+      setTimeout(() => {
+        fetch(`/emails/${each_email.id}`)
+        .then(response => response.json())
+        .then(email => {
+          console.log(email)
+          //Need to format each_email
+          const element = document.createElement('div');
+          element.innerHTML = `<strong>${email.sender}</strong> ${email.subject} ${email.timestamp}`;
+          element.addEventListener('click', function() {
+            load_email_page(email); 
+          });
+          document.querySelector('#emails-view').append(element);
+        });
+      }, index * 70);
     });
   });
 }
+
+function load_email_page(email) {
+  //Make a Function to show email page when clicked
+  //pass
+};
