9f07b18a07b8f789f5de23c6a744629c318971e4
Ayrton Machado <ayrtonsoaresm@gmail.com>
2024-09-04 20:06:52 -0300

    add function: read_email - when email as read show gray background, put content in email_page

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -71,14 +71,20 @@ function load_mailbox(mailbox) {
           //Need to finish format each_email
           const element = document.createElement('div');
           element.style.cursor = 'pointer';
-          element.className = 'email_container';
-          element.innerHTML = `<strong>${email.sender}</strong> ${email.subject} ${email.timestamp}`;
+          if (email.read === true) {
+            element.className = 'email_read email_container';
+          }
+          else {
+            element.className = 'email_container';
+          };
+          element.innerHTML = `<strong> ${email.sender} </strong> ${email.subject} ${email.timestamp}`;
           element.addEventListener('click', () => {
+            read_email(email);
             load_email_page(email);
           });
           document.querySelector('#emails-view').append(element);
         });
-      }, index * 70);
+      }, index * 50);
     });
   });
 }
@@ -88,23 +94,44 @@ function load_email_page(email) {
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
-  //email page
-  const email_page_content = `
+  //email header
+  const email_header = `
   <p><strong>From:</strong> ${email.recipients}</p>
   <p><strong>To:</strong> ${email.sender}</p> 
   <p><strong>Subject:</strong> ${email.subject}</p> 
-  </p><strong>Timestamp:</strong> ${email.timestamp}</p>
+  <p><strong>Timestamp:</strong> ${email.timestamp}</p>
   `;
-
-  //insert in html
-  document.querySelector('#emails-view').innerHTML = email_page_content
+  document.querySelector('#emails-view').innerHTML = email_header
+  
+  //reply button
   const reply_button = document.createElement('button');
   reply_button.innerHTML = 'Reply';
-  reply_button.className = 'btn btn-sm btn-outline-primary';
-
+  reply_button.className = 'reply_btn btn btn-sm btn-outline-primary';
   document.querySelector('#emails-view').append(reply_button);
+
+  //line before content
+  const end_header = document.createElement('hr');
+  document.querySelector('#emails-view').append(end_header);
+
+  //email content
+  const email_body = document.createElement('div');
+  email_body.className = 'email_body'
+  email_body.innerHTML = `${email.body}`
+  document.querySelector('#emails-view').append(email_body);
+
+  //reply button redirect to compose
+  document.querySelector('.reply_btn').addEventListener('click', compose_email); //need to fill 'to' space with email who sent
 
   //show the email in the console when it is clicked
   console.log(email);
 
+}
+
+function read_email(email) {
+  fetch(`/emails/${email.id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      read: true
+    })
+  })
 }
